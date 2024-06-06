b7c336620dff24eaa87686a6cb97826c2940380a
Дмитрий Карпухин <warlock@WRLCK.local>
2024-06-07 02:11:35 +0300

    css improve

--- devops_landing/main/admin.py
+++ devops_landing/main/admin.py
@@ -1,5 +1,7 @@
-# main/admin.py
 from django.contrib import admin
 from .models import Project
 
-admin.site.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'github_link', 'created_at', 'updated_at')
+
+admin.site.register(Project, ProjectAdmin)
--- devops_landing/main/models.py
+++ devops_landing/main/models.py
@@ -1,10 +1,11 @@
-# main/models.py
 from django.db import models
 from django.core.cache import cache
 
 class Project(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
+    github_link = models.URLField(blank=True, null=True)
+    image = models.ImageField(upload_to='projects/', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -14,7 +15,7 @@ class Project(models.Model):
 
     def delete(self, *args, **kwargs):
         super().delete(*args, **kwargs)
-        cache.delete('all_projects')  # Removecche via delete
+        cache.delete('all_projects')  # Remove cache via delete
 
     def __str__(self):
-        return self.title   
+        return self.title

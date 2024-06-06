955684b2f2e56219fa2441ef9c0d6f12b8425a38
warck30 <warck30@gmail.com>
2024-06-06 22:36:56 +0000

    CSS fix

--- devops_landing/main/urls.py
+++ devops_landing/main/urls.py
@@ -1,10 +1,12 @@
 # main/urls.py
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('education-skills/', views.education_skills, name='education_skills'),
     path('projects/', views.projects, name='projects'),
     path('contacts/', views.contacts, name='contacts'),
-]
+] +  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
